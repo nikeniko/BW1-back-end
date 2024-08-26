b8cd69c076b5d403207a4c15947f52e7500c3b3b
ed-Luis00 <edinsonluis2014@gmail.com>
2024-08-26 11:16:32 +0200

    first commit

---
+++ src/main/java/luisquiroz/Application.java
@@ -0,0 +1,17 @@
+package luisquiroz;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Application {
+    public static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("BW-UrbanTransit");
+    public static void main(String[] args) {
+
+        EntityManager em = emf.createEntityManager();
+
+
+
+        System.out.println("Hello World!");
+    }
+}
