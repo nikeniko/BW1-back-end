f3511c267e3601612fd2bc38f9851981c725e664
Nicolas P. <168541456+nikeniko@users.noreply.github.com>
2024-08-28 16:31:17 +0200

    tessera dao aggiornata

--- src/main/java/UrbanTransit/DAO/TesseraDAO.java
+++ src/main/java/UrbanTransit/DAO/TesseraDAO.java
@@ -1,74 +1,55 @@
 package UrbanTransit.DAO;
 
 import UrbanTransit.entities.Tessera;
-import UrbanTransit.entities.Tratta;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
 
-import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public class TesseraDAO {
+    private EntityManager em;
 
-    private EntityManager entityManager;
-
-    public TesseraDAO(EntityManager entityManager) {
-        this.entityManager = entityManager;
+    public TesseraDAO(EntityManager em){
+        this.em = em;
     }
 
-    public void createTessera(Tessera tessera) {
-        EntityTransaction transaction = entityManager.getTransaction();
-        try {
-            transaction.begin();
-            entityManager.persist(tessera);
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction.isActive()) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
+    // Metodo per salvare una nuova tessera
+    public void salvaTessera(Tessera tessera) {
+        em.getTransaction().begin();
+        em.persist(tessera);
+        em.getTransaction().commit();
     }
 
-    public Tessera getTesseraById(UUID id) {
-        return entityManager.find(Tessera.class, id);
+    // Metodo per trovare una tessera per ID
+    public Tessera trovaTesseraPerId(UUID id) {
+        return em.find(Tessera.class, id);
     }
 
-    public void updateTessera(UUID id, Boolean nuovoStato_tessera, LocalDate nuovoData_inizio, LocalDate nuovoData_scadenza) {
-        EntityTransaction transaction = entityManager.getTransaction();
-        try {
-            transaction.begin();
-            Tessera tessera = entityManager.find(Tessera.class, id);
-            if (tessera != null) {
-                tessera.setStato_tessera(nuovoStato_tessera);
-                tessera.setData_inizio(nuovoData_inizio);
-                tessera.setData_scadenza(nuovoData_scadenza);
-                entityManager.merge(tessera);
-            }
-
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction.isActive()) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
+    // Metodo per trovare tutte le tessere
+    public List<Tessera> trovaTutteLeTessere() {
+        TypedQuery<Tessera> query = em.createQuery("SELECT t FROM Tessera t", Tessera.class);
+        return query.getResultList();
     }
 
-    public void deleteTessera(UUID id) {
-        EntityTransaction transaction = entityManager.getTransaction();
-        try {
-            transaction.begin();
-            Tessera tessera = entityManager.find(Tessera.class, id);
-            if (tessera != null) {
-                entityManager.remove(tessera);
-            }
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction.isActive()) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
+    // Metodo per aggiornare una tessera esistente
+    public void aggiornaTessera(Tessera tessera) {
+        em.getTransaction().begin();
+        em.merge(tessera);
+        em.getTransaction().commit();
+    }
+
+    // Metodo per eliminare una tessera
+    public void eliminaTessera(Tessera tessera) {
+        em.getTransaction().begin();
+        em.remove(em.contains(tessera) ? tessera : em.merge(tessera));
+        em.getTransaction().commit();
+    }
+
+    // Metodo per trovare tessere per stato (attiva/inattiva)
+    public List<Tessera> trovaTesserePerStato(boolean stato) {
+        TypedQuery<Tessera> query = em.createQuery("SELECT t FROM Tessera t WHERE t.stato_tessera = :stato", Tessera.class);
+        query.setParameter("stato", stato);
+        return query.getResultList();
     }
 }
